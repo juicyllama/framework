dbe9de4cfe8469b061016d3691c37f9751156634
Andrew Slack <me@andyslack.com>
2023-09-18 18:46:37 +0200

    Actions for dev

--- frontend/vue-dev/index.ts
+++ frontend/vue-dev/index.ts
@@ -1,7 +1,6 @@
-const eslint = require("./.eslintrc.js");
-const prettier = require("./.prettierrc.js");
-const vitest = require("./vitest.config.js");
-const tsconfig = require("./.tsconfig.js");
-module.exports = { eslint, prettier, vitest, tsconfig }
+const eslint = require('./.eslintrc.js')
+const prettier = require('./.prettierrc.js')
+const vitest = require('./vitest.config.js')
+module.exports = { eslint, prettier, vitest }
 
 // TSconfig not able to import into package.json yet - https://github.com/microsoft/TypeScript/issues/32830
