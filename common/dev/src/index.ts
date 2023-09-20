d60a92d7b2d41f536a66974918e13b84536db70e
Andrew Slack <me@andyslack.com>
2023-09-20 15:08:28 +0200

    Migration to monorepo

--- common/dev/src/index.ts
+++ common/dev/src/index.ts
@@ -1,4 +1,4 @@
-import * as eslint from './.eslintrc.js'
+import * as eslint from '../../../.eslintrc.js'
 import * as prettier from './.prettierrc.js'
 
 export { eslint, prettier }
--- frontend/vue-utils/.eslintrc.js
+++ frontend/vue-utils/.eslintrc.js
@@ -1,25 +1,8 @@
 module.exports = {
-	ignorePatterns: ['/.github/*', '/cypress/*', '/dist'],
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier', 'prettier'],
+	extends: '../../.eslintrc.js',
 	parserOptions: {
-		ecmaVersion: 'latest',
-		parser: '@typescript-eslint/parser',
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
 		sourceType: 'module',
-	},
-	parser: 'vue-eslint-parser',
-	plugins: ['vue', '@typescript-eslint', 'prettier'],
-	rules: {
-		'no-unused-vars': 'off',
-		'vue/require-valid-default-prop': 'off',
-		'vue/no-mutating-props': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'vue/multi-word-component-names': 'off',
-		'vue/no-v-model-argument': 'off',
 	},
 }
