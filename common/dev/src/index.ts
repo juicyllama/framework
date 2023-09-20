84fea7969c35360aabb52a1a7e7c0247c326438c
Andrew Slack <me@andyslack.com>
2023-09-20 15:20:08 +0200

    Migration to monorepo

--- common/dev/src/index.ts
+++
@@ -1,6 +0,0 @@
-import * as eslint from '../../../.eslintrc.js'
-import * as prettier from './.prettierrc.js'
-
-export { eslint, prettier }
-
-// TSconfig not able to import into package.json yet - https://github.com/microsoft/TypeScript/issues/32830
