615fccdf92b30c2f18c6a3bf0a02a2885d42ff90
Andrew Slack <me@andyslack.com>
2023-09-22 14:25:39 +0200

    Push out utils update

--- common/.eslintrc.js
+++ common/.eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
-	extends: [
-		'../.eslintrc.js',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended'
-	],
+	extends: ['../.eslintrc.js', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		project: 'tsconfig.json',
 		sourceType: 'module',
 	},
-	root: true,
 	env: {
 		node: true,
 		jest: true,
