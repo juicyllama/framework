7177524eaf33f9548af0d25ba6c4b15d1b0d6b1c
Andrew Slack <me@andyslack.com>
2023-09-24 13:35:57 +0200

    Back to jest :(

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,12 +5,13 @@ export default defineConfig({
 	test: {
 		globals: true,
 		root: './src',
+		hookTimeout: 60000,
 	},
 	plugins: [
 		// This is required to build the test files with SWC
 		swc.vite({
 			// Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
-			module: { type: 'es6' },
+			module: { type: 'commonjs' },
 		}),
 	],
 })
