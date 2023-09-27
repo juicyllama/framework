27bc7836139221d44164ae988a2dba8d747899d4
Andrew Slack <me@andyslack.com>
2023-09-27 17:05:40 +0200

    Migrations back

--- backend/core/src/configs/database.config.ts
+++ backend/core/src/configs/database.config.ts
@@ -12,28 +12,28 @@ const DATABASE_SHARED = {
 	port: +process.env.MYSQL_PORT,
 	username: process.env.MYSQL_USERNAME,
 	password: process.env.MYSQL_PASSWORD,
-	autoLoadEntities: true,
 	namingStrategy: new CustomNamingStrategy(),
 	keepConnectionAlive: true,
-	charset: 'utf8mb4_0900_ai_ci'
+	charset: 'utf8mb4_0900_ai_ci',
+	autoLoadEntities: true,
+	entities: [
+		path.resolve(process.cwd(), 'dist', '**', '*.entity.js'),
+		path.resolve(process.cwd(), 'node_modules', '@juicyllama', '**', '*.entity.js'),
+	],
+	autoLoadMigrations: false, //should be false, otherwise long running migration will cause issues with microservices booting up
+	migrations: [path.resolve(process.cwd(), 'dist', 'db', 'migrations', '*.js')],
 }
 
 export const DATABASE = {
 	...DATABASE_SHARED,
 	database: process.env.MYSQL_DB_NAME,
-	entities: [
-		path.resolve(process.cwd(), 'dist', '**', '*.entity.{ts,js}'),
-		path.resolve(process.cwd(), 'node_modules', '@juicyllama', '**', '*.entity.{ts,js}'),
-	],
-	//synchronize: true, //becuase TypeORM migrations are pants !!
-	synchronize: false, //temp disable to get hive up and running
+	synchronize: false,
 	debug: Env.IsNotProd(),
 }
 
 export const TEST_DATABASE = {
 	...DATABASE_SHARED,
 	database: process.env.MYSQL_DB_TEST_NAME,
-	entities: [path.resolve(process.cwd(), '**', '*.entity.{ts,js}')],
 	synchronize: true,
 }
 
