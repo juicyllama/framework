ae8471788c38a8ac951e9ed0fee844298b7c0e08
Andrew Slack <me@andyslack.com>
2023-10-11 06:32:12 +0100

    Fix lint

--- frontend/core/src/components/common/menu/menu.vue
+++ frontend/core/src/components/common/menu/menu.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import MenuItem from './components/menu-item.vue'
 import { useRoute } from 'vue-router'
 import { Menu } from '@/types/common'
