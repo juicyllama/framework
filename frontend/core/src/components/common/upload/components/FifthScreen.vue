784b91a393ffb90fec64d5435e7b99d4f5fc3c9b
M Kuznetcov <michail.kuznetsov@gmail.com>
2023-11-07 08:42:20 +0100

    remove unused ref

--- frontend/core/src/components/common/upload/components/FifthScreen.vue
+++ frontend/core/src/components/common/upload/components/FifthScreen.vue
@@ -39,7 +39,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
 import { useUploaderStore } from '../../../../store/uploader'
 
 type uploadResult = {
