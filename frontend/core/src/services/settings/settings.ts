7592e3ba78b6214a4728d60d6a5f15231ac7915f
Andrew Slack <me@andyslack.com>
2023-11-28 15:50:09 +0100

    Adding settings service

---
+++ frontend/core/src/services/settings/settings.ts
@@ -0,0 +1,7 @@
+import { Api } from '../../helpers'
+import { Setting } from '../../types'
+
+type T = Setting
+export const SETTINGS_ENDPOINT = '/settings'
+export const SETTINGS_PUSHER_EVENT = 'account_${account_id}_settings'
+export class SettingsService extends Api<T> {}
--- frontend/core/src/types/settings.ts
+++ frontend/core/src/types/settings.ts
@@ -1,3 +1,10 @@
+export interface Setting {
+	key: string
+	value: any
+	account_id?: number
+	user_id?: number
+}
+
 export interface Settings {
 	return_path?: string
 }
