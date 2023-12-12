0c5efc846ccc179f62fcd7514ecbd9bb1451d434
Jay McDoniel <jmcdo29@gmail.com>
2023-12-12 14:14:15 -0800

    chore: add provider module and decorator to package exports

--- apps/shopify/src/modules/shopify.module.ts
+++ apps/shopify/src/modules/shopify.module.ts
@@ -3,11 +3,13 @@ import { Logger } from '@juicyllama/utils'
 import { Module } from '@nestjs/common'
 import { ShopifyAuthModule } from './auth/auth.module'
 import { ShopifyOrdersModule } from './orders/orders.module'
+import { ShopifyProviderModule } from './provider/provider.module'
 import { ShopifyInstallationService } from './shopify.installation'
 import { ShopifyWebhooksModule } from './webhooks/webhooks.module'
 
 @Module({
-	imports: [AppsModule, ShopifyAuthModule, ShopifyOrdersModule, ShopifyWebhooksModule],
+	imports: [AppsModule, ShopifyAuthModule, ShopifyOrdersModule, ShopifyWebhooksModule, ShopifyProviderModule],
 	providers: [ShopifyInstallationService, Logger],
+	exports: [ShopifyProviderModule],
 })
 export class ShopifyModule {}
