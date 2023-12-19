9b53fda62c76fbb8463d855a88fffc92be60c3f5
Jay McDoniel <jmcdo29@gmail.com>
2023-12-19 14:42:20 -0800

    refactor(aws): udpate s3 module to use new config approach

--- apps/aws/src/modules/s3/aws.s3.enums.ts
+++ apps/aws/src/modules/s3/aws.s3.enums.ts
@@ -1,10 +1,8 @@
-
 export type AwsS3Bucket = AwsS3BucketType.PUBLIC | AwsS3BucketType.PRIVATE | string
 
 export enum AwsS3BucketType {
 	PUBLIC = 'PUBLIC',
 	PRIVATE = 'PRIVATE',
-
 }
 
 export enum AwsS3Format {
--- apps/aws/src/modules/s3/config/aws.s3.config.joi.ts
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi'
-
-export const awsS3ConfigJoi = {
-	AWS_S3_JL_PUBLIC_BUCKET: Joi.string().optional(),
-	AWS_S3_JL_PRIVATE_BUCKET: Joi.string().optional(),
-	AWS_S3_JL_REGION: Joi.string().optional(),
-}
