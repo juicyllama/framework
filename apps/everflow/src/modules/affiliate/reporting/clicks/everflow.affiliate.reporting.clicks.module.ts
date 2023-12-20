d0e50224fc395784efc9ce042c10f6169d91ea66
Jay McDoniel <jmcdo29@gmail.com>
2023-12-20 10:28:04 -0800

    refactor(everflow): update reporting conversions to not use the config module

--- apps/everflow/src/modules/affiliate/reporting/clicks/everflow.affiliate.reporting.clicks.module.ts
+++ apps/everflow/src/modules/affiliate/reporting/clicks/everflow.affiliate.reporting.clicks.module.ts
@@ -1,11 +1,8 @@
-import { ConfigValidationModule } from '@juicyllama/core'
 import { Api, Logger } from '@juicyllama/utils'
 import { Module } from '@nestjs/common'
-import { EverflowConfigDto } from '../../../../config/everflow.config.dto'
 import { EverflowAffiliateReportingClicksService } from './everflow.affiliate.reporting.clicks.service'
 
 @Module({
-	imports: [ConfigValidationModule.register(EverflowConfigDto)],
 	controllers: [],
 	providers: [EverflowAffiliateReportingClicksService, Logger, Api],
 	exports: [EverflowAffiliateReportingClicksService],
