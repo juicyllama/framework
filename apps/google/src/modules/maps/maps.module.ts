4be3014cccd9df0e36537f34f4982bb400f9e8c4
Jay McDoniel <jmcdo29@gmail.com>
2023-12-20 11:28:40 -0800

    refactor(google): clean up base google module

--- apps/google/src/modules/maps/maps.module.ts
+++ apps/google/src/modules/maps/maps.module.ts
@@ -1,24 +1,10 @@
-import { forwardRef, Module } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { ConfigModule } from '@nestjs/config'
 import { mongodbConfig } from '@juicyllama/core'
-import { Env } from '@juicyllama/utils'
-import Joi from 'joi'
 import { PlacesModule } from './places/places.module'
 import { GeocodingModule } from './geocoding/geocoding.module'
-import googleConfig from '../../config/google.config'
-import { googleConfigJoi } from '../../config/google.config.joi'
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			isGlobal: true,
-			load: [mongodbConfig, googleConfig],
-			validationSchema: Env.IsNotTest() ? Joi.object(googleConfigJoi) : null,
-		}),
-		TypeOrmModule.forRootAsync(mongodbConfig()),
-		forwardRef(() => GeocodingModule),
-		forwardRef(() => PlacesModule),
-	],
+	imports: [TypeOrmModule.forRootAsync(mongodbConfig()), GeocodingModule, PlacesModule],
 })
 export class GoogleMapsModule {}
