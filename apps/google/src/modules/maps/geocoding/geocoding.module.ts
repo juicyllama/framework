7dc6a381537f0b8b0bd5599a8f734cdab1846f35
Jay McDoniel <jmcdo29@gmail.com>
2023-12-20 11:05:36 -0800

    refactor(google): create maps client provider and remove forward refs

--- apps/google/src/modules/maps/geocoding/geocoding.module.ts
+++ apps/google/src/modules/maps/geocoding/geocoding.module.ts
@@ -1,26 +1,28 @@
+import { Client } from '@googlemaps/google-maps-services-js'
+import { ConfigValidationModule, MONGODB, Query } from '@juicyllama/core'
+import { Logger } from '@juicyllama/utils'
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { ConfigModule } from '@nestjs/config'
-import { MONGODB, mongodbConfig, Query } from '@juicyllama/core'
-import { Api, Env, Logger } from '@juicyllama/utils'
-import Joi from 'joi'
-import { GeocodingService } from './geocoding.service'
-import googleConfig from '../../../config/google.config'
-import { googleConfigJoi } from '../../../config/google.config.joi'
+import { GoogleConfigDto } from '../../../config/google.config.dto'
 import { GoogleMapsGeocoding } from './geocoding.entity.mongo'
+import { GeocodingService } from './geocoding.service'
+import { GoogleMapsClientToken } from './geocoding.constants'
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({
-			isGlobal: true,
-			load: [mongodbConfig, googleConfig],
-			validationSchema: Env.IsNotTest() ? Joi.object(googleConfigJoi) : null,
-		}),
-		TypeOrmModule.forRootAsync(mongodbConfig()),
+		ConfigValidationModule.register(GoogleConfigDto),
 		TypeOrmModule.forFeature([GoogleMapsGeocoding], MONGODB),
 	],
 	controllers: [],
-	providers: [GeocodingService, Logger, Query, Api],
+	providers: [
+		GeocodingService,
+		Logger,
+		Query,
+		{
+			provide: GoogleMapsClientToken,
+			useValue: new Client(),
+		},
+	],
 	exports: [GeocodingService],
 })
 export class GeocodingModule {}
