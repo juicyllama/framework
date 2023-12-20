65dba01a75fc94b19672dafcde34781975d25c5f
Jay McDoniel <jmcdo29@gmail.com>
2023-12-19 17:07:42 -0800

    refactor(aws): update secrets module to use newer config approach
    Also created a secrets provider, and moved methods from aws.secrets.node to the
    secrets service

--- apps/aws/src/modules/secrets/aws.secrets.service.ts
+++ apps/aws/src/modules/secrets/aws.secrets.service.ts
@@ -1,13 +1,13 @@
-import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager'
-import { Logger, Env } from '@juicyllama/utils'
+import { SecretsManagerClient, GetSecretValueCommand, ListSecretsCommand } from '@aws-sdk/client-secrets-manager'
+import { Logger } from '@juicyllama/utils'
 import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+import { InjectSecretsManager } from './aws.secrets.constants'
 
 @Injectable()
 export class AwsSecretsService {
 	constructor(
-		private readonly configService: ConfigService,
 		private readonly logger: Logger,
+		@InjectSecretsManager() private readonly secretsClient: SecretsManagerClient,
 	) {}
 
 	/**
@@ -22,18 +22,7 @@ export class AwsSecretsService {
 		this.logger.debug(`[${domain}] Get Secrets`, secret_name)
 
 		try {
-			const client = new SecretsManagerClient({
-				region: this.configService.get<string>('aws.AWS_S3_JL_REGION'),
-				credentials: Env.IsProd
-					? undefined
-					: {
-							// we don't have/need these in AWS
-							accessKeyId: this.configService.get<string>('aws.AWS_JL_ACCESS_KEY_ID'),
-							secretAccessKey: this.configService.get<string>('aws.AWS_JL_SECRET_KEY_ID'),
-						},
-			})
-
-			const response = await client.send(
+			const response = await this.secretsClient.send(
 				new GetSecretValueCommand({
 					SecretId: secret_name,
 					VersionStage: 'AWSCURRENT', // VersionStage defaults to AWSCURRENT if unspecified
@@ -41,6 +30,57 @@ export class AwsSecretsService {
 			)
 
 			return JSON.parse(response.SecretString)
+		} catch (e) {
+			this.logger.error(
+				`[${domain}] Error: ${e.message}`,
+				e.response
+					? {
+							status: e.response.status,
+							data: e.response.data,
+						}
+					: null,
+			)
+			return
+		}
+	}
+
+	async getSecret(secret_name: string): Promise<any> {
+		const domain = 'app::aws::AwsSecretsService::getSecrets'
+
+		this.logger.debug(`[${domain}] Get Secrets: `, secret_name)
+
+		try {
+			const response = await this.secretsClient.send(
+				new GetSecretValueCommand({
+					SecretId: secret_name,
+					VersionStage: 'AWSCURRENT', // VersionStage defaults to AWSCURRENT if unspecified
+				}),
+			)
+
+			return JSON.parse(response.SecretString)
+		} catch (e) {
+			this.logger.error(
+				`[${domain}] Error: ${e.message}`,
+				e.response
+					? {
+							status: e.response.status,
+							data: e.response.data,
+						}
+					: null,
+			)
+			return
+		}
+	}
+
+	async listSecrets(): Promise<object> {
+		const domain = 'app::aws::AwsSecretsService::getSecrets'
+
+		this.logger.debug(`[${domain}] List Secrets: `)
+
+		try {
+			const response = await this.secretsClient.send(new ListSecretsCommand({}))
+
+			return response.SecretList
 		} catch (e) {
 			this.logger.error(
 				`[${domain}] Error: ${e.message}`,
