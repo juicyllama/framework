ab200e7ef5adf52ebed13595af38d0a86a1fcc99
Jay McDoniel <jmcdo29@gmail.com>
2023-12-19 17:07:42 -0800

    refactor(aws): update secrets module to use newer config approach
    Also created a secrets provider, and moved methods from aws.secrets.node to the
    secrets service

--- apps/aws/src/modules/secrets/aws.secrets.module.ts
+++ apps/aws/src/modules/secrets/aws.secrets.module.ts
@@ -1,21 +1,36 @@
+import { SecretsManagerClient } from '@aws-sdk/client-secrets-manager'
+import { Env, Logger } from '@juicyllama/utils'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { Api, Enviroment, Logger } from '@juicyllama/utils'
-import Joi from 'joi'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { AwsSecretsManagerToken } from './aws.secrets.constants'
 import { AwsSecretsService } from './aws.secrets.service'
-import { awsConfigJoi } from '../aws.config.joi'
-import awsConfig from '../aws.nest.config'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			load: [awsConfig],
-			validationSchema: process.env.NODE_ENV !== Enviroment.test ? Joi.object({ ...awsConfigJoi }) : null,
 		}),
 	],
 	controllers: [],
-	providers: [AwsSecretsService, Api, Logger],
+	providers: [
+		AwsSecretsService,
+		Logger,
+		{
+			provide: AwsSecretsManagerToken,
+			inject: [ConfigService],
+			useFactory: (config: ConfigService) => {
+				return new SecretsManagerClient({
+					region: config.get('AWS_S3_JL_REGION'),
+					credentials: Env.IsProd
+						? undefined
+						: {
+								accessKeyId: config.get('AWS_JL_ACCESS_KEY_ID'),
+								secretAccessKey: config.get('AWS_JL_SECRET_KEY_ID'),
+							},
+				})
+			},
+		},
+	],
 	exports: [AwsSecretsService],
 })
 export class AwsSecretsModule {}
