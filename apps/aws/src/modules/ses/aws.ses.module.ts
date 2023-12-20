681405759a30b914709eb0eb6437b82965e3d27a
Jay McDoniel <jmcdo29@gmail.com>
2023-12-19 17:43:30 -0800

    refactor(aws): update ses module to use the new config valdiation module

--- apps/aws/src/modules/ses/aws.ses.module.ts
+++ apps/aws/src/modules/ses/aws.ses.module.ts
@@ -1,27 +1,38 @@
+import { SESv2Client } from '@aws-sdk/client-sesv2'
+import { ConfigValidationModule, getConfigToken } from '@juicyllama/core'
+import { Logger, Markdown } from '@juicyllama/utils'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { Api, Enviroment, Logger, Markdown } from '@juicyllama/utils'
-import Joi from 'joi'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { AwsSesClientToken } from './aws.ses.constants'
 import { AwsSesService } from './aws.ses.service'
-import { awsConfigJoi } from '../aws.config.joi'
-import awsConfig from '../aws.nest.config'
-import { awsSesConfigJoi } from './config/aws.ses.config.joi'
-import awsSesConfig from './config/aws.ses.nest.config'
-import { systemConfigJoi, systemConfig } from '@juicyllama/core'
+import { AwsSesConfigDto } from './config/aws.ses.config.dto'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			load: [awsConfig, awsSesConfig, systemConfig],
-			validationSchema:
-				process.env.NODE_ENV !== Enviroment.test
-					? Joi.object({ ...awsConfigJoi, ...awsSesConfigJoi, ...systemConfigJoi })
-					: null,
 		}),
+		ConfigValidationModule.register(AwsSesConfigDto),
 	],
 	controllers: [],
-	providers: [AwsSesService, Api, Logger, Markdown],
+	providers: [
+		AwsSesService,
+		Logger,
+		Markdown,
+		{
+			provide: AwsSesClientToken,
+			inject: [getConfigToken(AwsSesConfigDto), ConfigService],
+			useFactory: (sesConfig: AwsSesConfigDto, config: ConfigService) => {
+				return new SESv2Client({
+					region: sesConfig.AWS_SES_JL_REGION ?? config.get('AWS_JL_REGION'),
+					credentials: {
+						accessKeyId: config.get('AWS_JL_ACCESS_KEY_ID'),
+						secretAccessKey: config.get('AWS_JL_SECRET_KEY_ID'),
+					},
+				})
+			},
+		},
+	],
 	exports: [AwsSesService],
 })
 export class AwsSesModule {}
--- apps/aws/src/modules/ses/config/aws.ses.config.joi.ts
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi'
-
-export const awsSesConfigJoi = {
-	AWS_SES_JL_REGION: Joi.string().default('eu-west-2'),
-	AWS_SES_JL_TEMPLATE_ARN: Joi.string(),
-}
