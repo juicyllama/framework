469cdb2417c6474a0326410fe7c86d9480d6632c
Jay McDoniel <jmcdo29@gmail.com>
2023-12-19 18:06:25 -0800

    refactor(aws): finalize the use of the new config module in the aws services

--- apps/aws/src/modules/s3/aws.s3.module.ts
+++ apps/aws/src/modules/s3/aws.s3.module.ts
@@ -2,31 +2,26 @@ import { S3Client as AWSS3Client } from '@aws-sdk/client-s3'
 import { ConfigValidationModule, getConfigToken } from '@juicyllama/core'
 import { Logger } from '@juicyllama/utils'
 import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
+import { AwsConfigDto } from '../aws.config.dto'
 import { S3Client } from './aws.s3.constants'
 import { AwsS3Service } from './aws.s3.service'
 import { AwsS3ConfigDto } from './config/aws.s3.config.dto'
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			isGlobal: true,
-		}),
-		ConfigValidationModule.register(AwsS3ConfigDto),
-	],
+	imports: [ConfigValidationModule.register(AwsS3ConfigDto), ConfigValidationModule.register(AwsConfigDto)],
 	controllers: [],
 	providers: [
 		AwsS3Service,
 		Logger,
 		{
 			provide: S3Client,
-			inject: [getConfigToken(AwsS3ConfigDto), ConfigService],
-			useFactory: (s3Config: AwsS3ConfigDto, config: ConfigService) => {
+			inject: [getConfigToken(AwsS3ConfigDto), getConfigToken(AwsConfigDto)],
+			useFactory: (s3Config: AwsS3ConfigDto, awsConfig: AwsConfigDto) => {
 				return new AWSS3Client({
 					region: s3Config.AWS_S3_JL_REGION,
 					credentials: {
-						accessKeyId: config.get('AWS_JL_ACCESS_KEY_ID'),
-						secretAccessKey: config.get('AWS_JL_SECRET_KEY_ID'),
+						accessKeyId: awsConfig.AWS_JL_ACCESS_KEY_ID,
+						secretAccessKey: awsConfig.AWS_JL_SECRET_KEY_ID,
 					},
 				})
 			},
