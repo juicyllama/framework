a824db20c9d8bbb42445bed56f92220efe78d118
Jay McDoniel <jmcdo29@gmail.com>
2023-12-21 09:28:56 -0800

    fix(google): update docs about database configuration and remove typeorm for root call

--- apps/google/src/modules/maps/maps.module.ts
+++ apps/google/src/modules/maps/maps.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { mongodbConfig } from '@juicyllama/core'
 import { PlacesModule } from './places/places.module'
 import { GeocodingModule } from './geocoding/geocoding.module'
 
 @Module({
-	imports: [TypeOrmModule.forRootAsync(mongodbConfig()), GeocodingModule, PlacesModule],
+	imports: [GeocodingModule, PlacesModule],
 })
 export class GoogleMapsModule {}
