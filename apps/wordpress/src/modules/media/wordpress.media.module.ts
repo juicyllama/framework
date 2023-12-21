0318998ba5047434d3191a1809943ecfba7395ab
Jay McDoniel <jmcdo29@gmail.com>
2023-12-21 13:26:44 -0800

    refactor(wordpress): media url provider and new config service

--- apps/wordpress/src/modules/media/wordpress.media.module.ts
+++ apps/wordpress/src/modules/media/wordpress.media.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common'
+import { ConfigValidationModule, getConfigToken } from '@juicyllama/core'
 import { Api, Logger } from '@juicyllama/utils'
+import { Module } from '@nestjs/common'
+import { WordpressConfigDto } from '../../config/wordpress.config.dto'
+import { WordpressMediaUrlToken } from './wordpress.media.constants'
 import { WordpressMediaService } from './wordpress.media.service'
 
 @Module({
-	providers: [WordpressMediaService, Logger, Api],
+	imports: [ConfigValidationModule.register(WordpressConfigDto)],
+	providers: [
+		WordpressMediaService,
+		Logger,
+		Api,
+		{
+			provide: WordpressMediaUrlToken,
+			inject: [getConfigToken(WordpressConfigDto)],
+			useFactory: (config: WordpressConfigDto) => `${config.WORDPRESS_URL}/wp-json/wp/v2/media`,
+		},
+	],
 	exports: [WordpressMediaService],
 })
 export class WordpressMediaModule {}
