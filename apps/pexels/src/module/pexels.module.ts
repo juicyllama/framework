ee42b9a3fd812144892426e3e4d1230991360190
Jay McDoniel <jmcdo29@gmail.com>
2023-12-21 10:03:54 -0800

    refactor(pexels): dedicated pexels provider and new config module

--- apps/pexels/src/module/pexels.module.ts
+++ apps/pexels/src/module/pexels.module.ts
@@ -1,23 +1,25 @@
-import { ConfigModule } from '@nestjs/config'
-import { Api, Enviroment } from '@juicyllama/utils'
-import { PexelsService } from './pexels.service'
-import Joi from 'joi'
+import { ConfigValidationModule, getConfigToken } from '@juicyllama/core'
 import { Logger } from '@juicyllama/utils'
 import { Module } from '@nestjs/common'
-import pexelsConfig from '../configs/pexels.config'
-import { pexelsConfigJoi } from '../configs/pexels.config.joi'
+import { createClient } from 'pexels'
+import { PexelsConfigDto } from '../configs/pexels.config.dto'
+import { PexelsClientToken } from './pexels.constants'
+import { PexelsService } from './pexels.service'
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			load: [pexelsConfig],
-			isGlobal: true,
-			envFilePath: '.env',
-			validationSchema: process.env.NODE_ENV !== Enviroment.test ? Joi.object(pexelsConfigJoi) : null,
-		}),
-	],
+	imports: [ConfigValidationModule.register(PexelsConfigDto)],
 	controllers: [],
-	providers: [PexelsService, Logger, Api],
+	providers: [
+		PexelsService,
+		Logger,
+		{
+			provide: PexelsClientToken,
+			inject: [getConfigToken(PexelsConfigDto)],
+			useFactory: (config: PexelsConfigDto) => {
+				return createClient(config.PEXELS_API_KEY)
+			},
+		},
+	],
 	exports: [PexelsService],
 })
 export class PexelsModule {}
