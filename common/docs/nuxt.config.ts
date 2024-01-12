47683e0b9d7e87220644ae69205115de058e5756
Andrew Slack <me@andyslack.com>
2024-01-12 16:44:36 +0000

    Migrated docs to nuxt ui-pro (WIP)

--- common/docs/nuxt.config.ts
+++ common/docs/nuxt.config.ts
@@ -11,7 +11,6 @@ export default defineNuxtConfig({
 	  'nuxt-og-image'
 	],
 	hooks: {
-	  // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
 	  'components:extend': (components) => {
 		const globals = components.filter((c) => ['UButton', 'UIcon'].includes(c.pascalName))
   
