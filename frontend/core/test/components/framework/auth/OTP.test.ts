d5c7dd0465b279363f7e77f4ddbef45307a724d2
Yossi Shasho <jozard00@gmail.com>
2024-01-23 19:02:18 +0200

    test fix try

--- frontend/core/test/components/framework/auth/OTP.test.ts
+++ frontend/core/test/components/framework/auth/OTP.test.ts
@@ -1,16 +1,15 @@
+import { it, describe, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import { describe, expect, it } from 'vitest'
 import OTP from '../../../../src/components/core/auth/OTP.vue'
 
 describe('OTP', () => {
 	const wrapper = mount(OTP)
 	it('renders six input fields', () => {
-		expect(wrapper.findAll('[input-classes=otp-input]').length).toBe(6)
+		expect(wrapper.findAll('.otp-input').length).toBe(6)
 	})
-	// FIXME the OTP doesn't render regular fields now, thus the test cannot use setValue to fill the fields. Need to find a way to do it.
-	it.skip('emits complete when filled', async () => {
-		for (const field of wrapper.findAll('[input-classes=otp-input]')) {
-			const index = wrapper.findAll('[input-classes=otp-input]').indexOf(field)
+	it('emits complete when filled', async () => {
+		for (const field of wrapper.findAll('.otp-input')) {
+			const index = wrapper.findAll('.otp-input').indexOf(field)
 			await field.setValue(index + '')
 		}
 		expect(wrapper.emitted('complete')).toBeTruthy()
