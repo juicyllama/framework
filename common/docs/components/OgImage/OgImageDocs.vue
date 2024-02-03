a7ceb11cfe7590e2258a7878a9ed1cf52f18272c
M Kuznetcov <michail.kuznetsov@gmail.com>
2024-02-03 12:38:13 +0100

    remove unused

--- common/docs/components/OgImage/OgImageDocs.vue
+++ common/docs/components/OgImage/OgImageDocs.vue
@@ -16,7 +16,8 @@ defineProps({
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col justify-center text-center bg-slate-900 p-8">
+  <div :style="{backgroundImage: 'https://juicyllama.com/assets/images/logo-35x35-primary.png', backgroundPosition: 'top right', backgroundRepeat: 'no-repeat'}"
+	class="w-full h-full flex flex-col justify-center text-center bg-slate-900 p-8">
     <div class="relative">
       <h1 class="text-8xl mb-4 text-white">
         {{ title }}
