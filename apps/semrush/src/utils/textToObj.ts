fd878c6724ca9edd4cf569c6e0ce9d7343e92b34
Nathan Gemis <nathangemis@gmail.com>
2024-02-05 00:07:16 +0100

    create camelcase json response

--- apps/semrush/src/utils/textToObj.ts
+++ apps/semrush/src/utils/textToObj.ts
@@ -1,11 +1,15 @@
 export function parseTextData(input: string): object[] {
 	const lines = input.trim().split('\n')
-	const headers = lines[0].split(';')
+	const headers = lines[0].split(';').map(header => header.replace(/[\r\n]+/g, '')) // Clean line symbols in headers
 	const data = lines.slice(1).map(line => {
-		const values = line.split(';')
+		const values = line.split(';').map(value => value.replace(/[\r\n]+/g, '')) // Clean line symbols in each value
 		const obj: any = {}
 		headers.forEach((header, index) => {
-			obj[header] = values[index]
+			// transform key to camelCase
+			let key = header.replace(/[^a-zA-Z0-9]+(.)/g, (_match, chr) => chr.toUpperCase())
+			key = key.charAt(0).toLowerCase() + key.slice(1)
+
+			obj[key] = values[index]
 		})
 		return obj
 	})
