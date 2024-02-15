18fb459a26242b0a260d523fb00b594b71dae50a
M Kuznetcov <michail.kuznetsov@gmail.com>
2024-02-15 01:36:47 +0100

    add markup component for TODOs

---
+++ common/docs/components/content/Incomplete.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h5>Incomplete</h5>
+    <p>This documentation section needs some work. Do you know what else needs to be taken into account in this module API?
+	<br> Let us know on <a href="“https://github.com/juicyllama/framework/discussions”">GitHub</a> or <a href="“https://discord.com/invite/KGv76Q9S3s”">Discord</a>.</p>
+  </div>
+</template>
