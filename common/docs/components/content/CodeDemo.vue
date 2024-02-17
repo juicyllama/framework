43e319ccdc604cf419199f443138787c43fd6b62
M Kuznetcov <michail.kuznetsov@gmail.com>
2024-02-17 11:35:15 +0100

    display link on mobile

--- common/docs/components/content/CodeDemo.vue
+++ common/docs/components/content/CodeDemo.vue
@@ -1,6 +1,11 @@
 <template>
-  <ClientOnly>
+  <ClientOnly v-if="!isMobile">
     <embed type="text/html" :src="props.demo" width="100%" height="500">
+  </ClientOnly>
+  <ClientOnly v-else>
+    <div class="flex justify-center items-center">
+      <a :href="props.demo" target="_blank" type="button" class="button-primary">Open on Stackblitz.com</a>
+    </div>
   </ClientOnly>
 </template>
 <script setup lang="ts">
@@ -9,4 +14,15 @@ const props = defineProps({
 	// code: String,
 	demo: String,
 })
+
+const isMobile = ref(false);
+
+onMounted(() => {
+	updateWidth();
+	window.addEventListener('resize', updateWidth);
+});
+
+const updateWidth = () => {
+	isMobile.value = window.innerWidth <= 640;
+};
 </script>
