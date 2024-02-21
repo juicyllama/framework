7fdd39273aec20b230dc08dba5b8e7cac67cbad1
Andrew Slack <me@andyslack.com>
2024-02-21 08:08:32 +0100

    Add markAsRead functionality and update API headers

--- frontend/core/src/services/social/chat.ts
+++ frontend/core/src/services/social/chat.ts
@@ -1,3 +1,5 @@
+import { accountStore } from 'src'
+import instance from '..'
 import { Api } from '../../helpers'
 import { Chat } from '../../types/social/chat'
 
@@ -11,18 +13,21 @@ export class SocialChatService extends Api<any> {
 	  }
 
 	async getUnreadChatsCount(): Promise<number> {
+		instance.defaults.headers.common['account-id'] = accountStore.selected_account?.account_id
 		return await super.get({
 			url: `${SOCIAL_CHAT_ENDPOINT}/unread`
 		}) as any
 	}
 
 	async listChats(): Promise<SOCIAL_CHAT_T[]> {
+		instance.defaults.headers.common['account-id'] = accountStore.selected_account?.account_id
 		return await super.get({
 			url: SOCIAL_CHAT_ENDPOINT
 		})
 	}
 
 	async markChatAsRead(chat_id: number): Promise<void> {
+		instance.defaults.headers.common['account-id'] = accountStore.selected_account?.account_id
 		await super.patch({
 			url: `${SOCIAL_CHAT_ENDPOINT}/${chat_id}/read`,
 			data: {}
