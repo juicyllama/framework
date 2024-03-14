917d36c57f32bba0e5536fbeb2d328b2e3d86a25
Yossi Shasho <jozard00@gmail.com>
2024-03-14 18:23:00 +0200

    Core: add Query#getRecordId method to fix TS build type errors

--- backend/core/src/utils/typeorm/Query.ts
+++ backend/core/src/utils/typeorm/Query.ts
@@ -277,7 +277,7 @@ export class Query<T extends ObjectLiteral> {
 			logger.debug(`[QUERY][UPDATE][${repository.metadata.tableName}]`, data)
 		}
 
-		if (!data[this.getPrimaryKey(repository)]) {
+		if (!this.getRecordId(repository, data)) {
 			throw new Error(
 				`Primary key ${<string>this.getPrimaryKey(repository)} missing from update to ${
 					repository.metadata.tableName
@@ -286,8 +286,8 @@ export class Query<T extends ObjectLiteral> {
 		}
 
 		try {
-			await repository.update(data[this.getPrimaryKey(repository)], <any>data)
-			return await this.findOneById(repository, data[this.getPrimaryKey(repository)], relations)
+			await repository.update(this.getRecordId(repository, data), <any>data)
+			return await this.findOneById(repository, this.getRecordId(repository, data), relations)
 		} catch (e) {
 			this.logUpdateError(e, repository, data)
 			throw e
@@ -551,6 +551,10 @@ export class Query<T extends ObjectLiteral> {
 		await this.raw(repository, sql_drop)
 	}
 
+	/**
+	 * @param repository
+	 * @returns The primary key's name of the table
+	 */
 	getPrimaryKey(repository: Repository<T>): keyof DeepPartial<T> {
 		const pk = repository.metadata.columns.find(column => {
 			if (column.isPrimary) {
@@ -561,6 +565,16 @@ export class Query<T extends ObjectLiteral> {
 			throw new Error('no primary key was found')
 		}
 		return <keyof DeepPartial<T>>pk
+	}
+
+	/**
+	 *
+	 * @param repository
+	 * @param record
+	 * @returns The primary key's value of the record
+	 */
+	getRecordId(repository: Repository<T>, record: DeepPartial<T>): number {
+		return record[this.getPrimaryKey(repository)] as unknown as number
 	}
 
 	getTableName(repository: Repository<T>) {
@@ -948,7 +962,7 @@ export class Query<T extends ObjectLiteral> {
 			try {
 				const r = await this.findOne(repository, <any>{
 					where: {
-						[dedup_field]: record[dedup_field  as keyof DeepPartial<T>],
+						[dedup_field]: record[dedup_field as keyof DeepPartial<T>],
 					},
 				})
 
