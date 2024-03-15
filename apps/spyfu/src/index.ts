65f3c86892780d02a406f92f31da8df29620c5f7
Nathan Gemis <nathangemis@gmail.com>
2024-03-15 13:07:29 +0100

    most valuable keyword endpoint

--- apps/spyfu/src/index.ts
+++ apps/spyfu/src/index.ts
@@ -3,5 +3,6 @@ import { CompetitorsService } from './module/competitors/competitors.service'
 import { DomainStatsService } from './module/domainStats/domainStats.service'
 import { KombatService } from './module/kombat/kombat.service'
 import { KeywordsService } from './module/keywords/keywords.service'
+import { SEOService } from './module/seo/seo.service'
 
-export { SpyfuModule, CompetitorsService, DomainStatsService, KombatService, KeywordsService }
+export { SpyfuModule, CompetitorsService, DomainStatsService, KombatService, KeywordsService, SEOService }
--- apps/spyfu/src/utils/intefaces.ts
+++ apps/spyfu/src/utils/intefaces.ts
@@ -9,7 +9,6 @@ export interface OrganicKombatKeywordParams {
 	countryCode: string
 	adultFilter: boolean
 }
-
 export interface LatestDomainStatsParams {
 	domain: string
 	countryCode: string
@@ -25,6 +24,27 @@ export interface DomainCompetitorParams {
 export interface KeywordsResearchParams {
 	keyword: string
 	countryCode: string
+}
+
+export interface MostValuableKeywordsParams {
+	query: string
+	sortBy: string
+	sortOrder: string
+	startingRow: number
+	pageSize: number
+	countryCode: string
+	adultFilter: string
+	onlyAdultKeywords: string
+	excludeHomepageKeywords: string
+	'keywordDifficulty.min': number
+	'keywordDifficulty.max': number
+	'adCount.min': number
+	'adCount.max': number
+	'searchVolume.min': number
+	'searchVolume.max': number
+	includeTerms: string
+	includeAnyTerm: string
+	excludeTerms: string
 }
 
 interface BaseResponse {
@@ -80,6 +100,31 @@ export interface IKeyword {
 	adPosition: number | null
 }
 
+export interface IMostValuableKeyword {
+	keyword: string
+	topRankedUrl: string
+	rank: number
+	rankChange: number
+	searchVolume: number
+	keywordDifficulty: number
+	broadCostPerClick: number
+	phraseCostPerClick: number
+	exactCostPerClick: number
+	seoClicks: number
+	seoClicksChange: number
+	totalMonthlyClicks: number
+	percentMobileSearches: number
+	percentDesktopSearches: number
+	percentNotClicked: number
+	percentPaidClicks: number
+	percentOrganicClicks: number
+	broadMonthlyCost: number
+	phraseMonthlyCost: number
+	exactMonthlyCost: number
+	paidCompetitors: number
+	rankingHomepages: number
+}
+
 export interface IResponse<T> extends BaseResponse {
 	results: T[]
 }
@@ -96,5 +141,9 @@ export interface IDomainCompetitorResponse extends BaseResponse, IResponse<IDoma
 }
 
 export interface IKeywordsResearchResponse extends BaseResponse, IResponse<IKeyword> {
+	totalMatchingResults: number
+}
+
+export interface IMostValuableKeywordsResponse extends BaseResponse, IResponse<IMostValuableKeyword> {
 	totalMatchingResults: number
 }
