7f2a79fdcdd403ba15ca48b9012e1b57094796f1
Andrew Slack <me@andyslack.com>
2024-03-18 16:39:50 +0100

    website bump

--- backend/websites/src/index.ts
+++ backend/websites/src/index.ts
@@ -1,32 +1,18 @@
 // Entities
-import { Website } from './modules/websites/websites.entity'
+export { Website } from './modules/websites/websites.entity'
 
 // Modules
-import { WebsitesModule } from './modules/websites/websites.module'
+export { WebsitesModule } from './modules/websites/websites.module'
 
 // Controllers
-import { WebsitesController } from './modules/websites/websites.controller'
+export { WebsitesController } from './modules/websites/websites.controller'
 
 // Services
-import { WebsitesService } from './modules/websites/websites.service'
+export { WebsitesService } from './modules/websites/websites.service'
 
 // Enums
-import { WebsiteSelect, WebsiteRelations, WebsiteOrderBy } from './modules/websites/websites.enums'
+export { WebsiteSelect, WebsiteRelations, WebsiteOrderBy } from './modules/websites/websites.enums'
 
 // DTOs
-import { WebsiteDto, CreateWebsiteDto, UpdateWebsiteDto } from './modules/websites/websites.dto'
-import { installWebsiteDocs } from './docs/install'
-
-export {
-	Website,
-	WebsitesModule,
-	WebsitesController,
-	WebsitesService,
-	WebsiteDto,
-	CreateWebsiteDto,
-	UpdateWebsiteDto,
-	WebsiteSelect,
-	WebsiteRelations,
-	WebsiteOrderBy,
-	installWebsiteDocs,
-}
+export { WebsiteDto, CreateWebsiteDto, UpdateWebsiteDto } from './modules/websites/websites.dto'
+export { installWebsiteDocs } from './docs/install'
