024a9a0cc6b6be5500fd409dbadbe0b4b752be77
Andrew Slack <me@andyslack.com>
2024-03-18 16:41:14 +0100

    CRM bump

--- backend/crm/src/index.ts
+++ backend/crm/src/index.ts
@@ -1,72 +1,38 @@
 // Entities
-import { Contact } from './modules/contacts/contacts.entity'
-import { ContactAddress } from './modules/contacts/address/address.entity'
-import { ContactEmail } from './modules/contacts/email/email.entity'
-import { ContactPhone } from './modules/contacts/phone/phone.entity'
-import { ContactSocial } from './modules/contacts/social/social.entity'
+export { Contact } from './modules/contacts/contacts.entity'
+export { ContactAddress } from './modules/contacts/address/address.entity'
+export { ContactEmail } from './modules/contacts/email/email.entity'
+export { ContactPhone } from './modules/contacts/phone/phone.entity'
+export { ContactSocial } from './modules/contacts/social/social.entity'
 
 // Modules
-import { CrmModule } from './crm.module'
-import { CrmCronsModule } from './modules/contacts/crm.crons.module'
-import { ContactsModule } from './modules/contacts/contacts.module'
+export { CrmModule } from './crm.module'
+export { CrmCronsModule } from './modules/contacts/crm.crons.module'
+export { ContactsModule } from './modules/contacts/contacts.module'
 
 // Controllers
-import { ContactsController } from './modules/contacts/contacts.controller'
-import { CRMCronsController } from './modules/contacts/crm.cron.controller'
+export { ContactsController } from './modules/contacts/contacts.controller'
+export { CRMCronsController } from './modules/contacts/crm.cron.controller'
 
 // Services
-import { ContactsService } from './modules/contacts/contacts.service'
-import { CrmCronsContactsService } from './modules/contacts/crm.crons.contacts.service'
-import { ContactAddressService } from './modules/contacts/address/address.service'
-import { ContactEmailService } from './modules/contacts/email/email.service'
-import { ContactPhoneService } from './modules/contacts/phone/phone.service'
-import { ContactSocialService } from './modules/contacts/social/social.service'
+export { ContactsService } from './modules/contacts/contacts.service'
+export { CrmCronsContactsService } from './modules/contacts/crm.crons.contacts.service'
+export { ContactAddressService } from './modules/contacts/address/address.service'
+export { ContactEmailService } from './modules/contacts/email/email.service'
+export { ContactPhoneService } from './modules/contacts/phone/phone.service'
+export { ContactSocialService } from './modules/contacts/social/social.service'
 
 // Enums
-import { ContactSelect, ContactRelations, ContactOrderBy } from './modules/contacts/contacts.enums'
-import { ContactAddressType } from './modules/contacts/address/address.enums'
-import { ContactEmailType } from './modules/contacts/email/email.enums'
-import { ContactPhoneType } from './modules/contacts/phone/phone.enums'
-import { ContactSocialType } from './modules/contacts/social/social.enums'
+export { ContactSelect, ContactRelations, ContactOrderBy } from './modules/contacts/contacts.enums'
+export { ContactAddressType } from './modules/contacts/address/address.enums'
+export { ContactEmailType } from './modules/contacts/email/email.enums'
+export { ContactPhoneType } from './modules/contacts/phone/phone.enums'
+export { ContactSocialType } from './modules/contacts/social/social.enums'
 
 // DTOs
-import { ContactDto, CreateContactDto, UpdateContactDto } from './modules/contacts/contacts.dto'
-import { ContactAddressDto } from './modules/contacts/address/address.dto'
-import { ContactEmailDto } from './modules/contacts/email/email.dto'
-import { ContactPhoneDto } from './modules/contacts/phone/phone.dto'
-import { ContactSocialDto } from './modules/contacts/social/social.dto'
-import { installCRMDocs } from './docs/install'
-
-export {
-	CrmModule,
-	CrmCronsModule,
-	ContactsModule,
-	ContactsController,
-	CRMCronsController,
-	ContactsService,
-	CrmCronsContactsService,
-	ContactDto,
-	CreateContactDto,
-	UpdateContactDto,
-	ContactSelect,
-	ContactRelations,
-	ContactOrderBy,
-	ContactAddressDto,
-	ContactEmailDto,
-	ContactPhoneDto,
-	ContactSocialDto,
-	ContactSocial,
-	ContactPhone,
-	ContactEmail,
-	Contact,
-	ContactAddress,
-	ContactSocialType,
-	ContactPhoneType,
-	ContactAddressType,
-	ContactEmailType,
-	installCRMDocs,
-	ContactSocialService,
-	ContactPhoneService,
-	ContactEmailService,
-	ContactAddressService,
-}
+export { ContactDto, CreateContactDto, UpdateContactDto } from './modules/contacts/contacts.dto'
+export { ContactAddressDto } from './modules/contacts/address/address.dto'
+export { ContactEmailDto } from './modules/contacts/email/email.dto'
+export { ContactPhoneDto } from './modules/contacts/phone/phone.dto'
+export { ContactSocialDto } from './modules/contacts/social/social.dto'
+export { installCRMDocs } from './docs/install'
