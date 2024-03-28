d62db7a5cb7f71640ab3d27a9f74d605bf5de52f
Andrew Slack <me@andyslack.com>
2024-03-28 17:52:23 +0000

    Adding logging to transactions cron

--- apps/shopify/src/modules/orders/orders.module.ts
+++ apps/shopify/src/modules/orders/orders.module.ts
@@ -1,31 +1,11 @@
-import { InstalledAppsModule, OAuthModule } from '@juicyllama/app-store'
-import { ContactsModule } from '@juicyllama/crm'
-import {
-	StoresModule,
-	TransactionDiscountsModule,
-	TransactionsModule,
-	TransactionItemsModule,
-	SkusModule,
-	BundlesModule,
-} from '@juicyllama/ecommerce'
+import { OAuthModule } from '@juicyllama/app-store'
 import { Logger } from '@juicyllama/utils'
 import { Module, forwardRef } from '@nestjs/common'
 import { ShopifyProviderModule } from '../provider/provider.module'
 import { ShopifyOrdersService } from './orders.service'
 
 @Module({
-	imports: [
-		forwardRef(() => BundlesModule),
-		forwardRef(() => ContactsModule),
-		forwardRef(() => OAuthModule),
-		forwardRef(() => InstalledAppsModule),
-		forwardRef(() => StoresModule),
-		forwardRef(() => TransactionsModule),
-		forwardRef(() => TransactionDiscountsModule),
-		forwardRef(() => TransactionItemsModule),
-		forwardRef(() => SkusModule),
-		forwardRef(() => ShopifyProviderModule),
-	],
+	imports: [forwardRef(() => OAuthModule), forwardRef(() => ShopifyProviderModule)],
 	controllers: [],
 	providers: [ShopifyOrdersService, Logger],
 	exports: [ShopifyOrdersService],
