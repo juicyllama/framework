bee8c475fbb6d7c78475033dd805bdf9852134f2
Andrew Slack <me@andyslack.com>
2024-04-19 14:35:43 +0200

    standadising store

--- frontend/core/src/store/websites.ts
+++ frontend/core/src/store/websites.ts
@@ -5,16 +5,62 @@ import { Website } from '../types'
 
 type T = Website
 const localStore = 'websites'
+const primayKey = 'website_id'
 
 export const WebsitesStore = defineStore(localStore, {
-	state: () => {
-		return <T[]>JSON.parse(window.localStorage.getItem(localStore) ?? '[]')
-	},
+	state: () => ({
+		[localStore]: window.localStorage.getItem(localStore)
+			? <T[]>JSON.parse(window.localStorage.getItem(localStore) ?? '[]')
+			: [],
+	}),
 	actions: {
+		async create(data: Partial<T>): Promise<T> {
+			const result = await service.create({
+				data
+			})
+			const results = <T[]>this.$state[localStore] ?? []
+			results.push(result)
+
+			this.$state[localStore] = results
+			window.localStorage.setItem(localStore, JSON.stringify(results))
+			return result
+		},
+		async findOne(record_id: number): Promise<T | undefined> {
+			const result = await service.findOne({record_id})
+			if(!result) return undefined
+			const results = <T[]>this.$state[localStore]
+			const index = results.findIndex((r) => r[primayKey] === result[primayKey])
+			results[index] = result
+			this.$state[localStore] = results
+			window.localStorage.setItem(localStore, JSON.stringify(results))
+			return result
+		},
 		async findAll(): Promise<T[]> {
 			const result = await service.findAll({})
 			window.sessionStorage.setItem(localStore, JSON.stringify(result))
 			this.$state[localStore] = result
+			return result
+		},
+		async update(data: Partial<T>): Promise<T> {
+			const result = await service.update({
+				data
+			})
+			const results = <T[]>this.$state[localStore]
+			const index = results.findIndex((r) => r[primayKey] === result[primayKey])
+			results[index] = result
+			this.$state[localStore] = results
+			window.localStorage.setItem(localStore, JSON.stringify(results))
+			return result
+		},
+		async delete(record_id: number): Promise<T> {
+			const result = await service.delete({
+				record_id
+			})
+			const results = <T[]>this.$state[localStore]
+			const index = results.findIndex((r) => r[primayKey] === result[primayKey])
+			results.splice(index, 1)
+			this.$state[localStore] = results
+			window.localStorage.setItem(localStore, JSON.stringify(results))
 			return result
 		},
 		/** depreciate for consistancy of function names (replaced by findAll) */
@@ -25,10 +71,12 @@ export const WebsitesStore = defineStore(localStore, {
 			}
 			return result
 		},
+		/** depreciate */
 		setWebsites(data: Website[]) {
 			window.localStorage.setItem('websites', JSON.stringify(data))
 			this.$state = data
 		},
+		/** depreciate */
 		async updateWebsite(website_id: number, data: Partial<Website>, $q?: QVueGlobals): Promise<Website> {
 			const result = await service.update({
 				url: WEBSITE_ENDPOINT + '/' + website_id,
@@ -47,7 +95,7 @@ export const WebsitesStore = defineStore(localStore, {
 
 			return result
 		},
-
+		/** depreciate */
 		async deleteWebsite(website_id: number, $q?: QVueGlobals): Promise<Website> {
 			const result = await service.delete({
 				url: WEBSITE_ENDPOINT + '/' + website_id,
@@ -62,13 +110,13 @@ export const WebsitesStore = defineStore(localStore, {
 
 			return result
 		},
-
+		/** depreciate */
 		getWebsiteById(website_id: number): Website | undefined {
 			return this.$state.find(website => website.website_id === website_id)
 		},
 	},
 	getters: {
-		get(state): T[] | null {
+		get(state): T[] {
 			return state[localStore]
 		},
 	},
