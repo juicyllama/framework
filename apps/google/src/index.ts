a4b544cd3f3401b6023caabb0ba722218e9a2846
Yossi Shasho <jozard00@gmail.com>
2024-05-10 21:33:50 +0300

    Try to fix error for missing core-js types

--- apps/google/src/index.ts
+++ apps/google/src/index.ts
@@ -8,5 +8,5 @@ export { GeocodingService } from './modules/maps/geocoding/geocoding.service'
 export { PlacesService } from './modules/maps/places/places.service'
 
 //Entity
-export { GoogleMapsGeocoding } from './modules/maps/geocoding/geocoding.entity.mongo'
 export { GoogleMapsPlace } from './modules/maps/places/places.entity.mongo'
+export { GoogleMapsGeocoding } from './modules/maps/geocoding/geocoding.entity.mongo'
