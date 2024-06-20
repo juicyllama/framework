69a62b6cacceefc5526ed3079c7c078ba502ac75
Andrew Slack <me@andyslack.com>
2024-06-20 06:12:01 +0200

    Improve docs

--- common/docs/pages/index/index.vue
+++ common/docs/pages/index/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import Hero from './components/Hero.vue'
 import Backend from './components/Backend.vue'
-import Frontend from './components/Frontend.vue'
 import Apps from './components/Apps.vue'
 import CTAs from './components/CTAs.vue'
 
